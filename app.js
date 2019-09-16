3f9aee611d32a3fd35d9a63242c0d1925df61184
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-09-16 16:10:51 -0300

    Added server_config.json

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ var formidable = require('formidable');
 var fs = require('fs');
 var localIpV4Address = require("local-ipv4-address");
 
+let rawdata = fs.readFileSync('server_config.json');
+let config = JSON.parse(rawdata);
+
 http.createServer(function (req, res) {
 	if (req.url == '/fileupload') {
 		var form = new formidable.IncomingForm();
 		form.parse(req, function (err, fields, files) {
 			var oldpath = files.filetoupload.path;
-			var newpath = 'C:/Users/Lucas Rocha/Desktop/lan uploader/fileupload/' + files.filetoupload.name;
+			var newpath = config.path + files.filetoupload.name;
 			process.stdout.write("\nFile received -> " + files.filetoupload.name);
 			fs.rename(oldpath, newpath, function (err) {
 				if (err) throw err;
 				res.writeHead(301, {
-					Location: 'localhost:3000'
+					Location: 'localhost:' + config.port
 				});
 				res.end();
 			});
@@ -27,7 +30,7 @@ http.createServer(function (req, res) {
 		var file = fs.createReadStream('lib/html/index.html');
 		file.pipe(res);
 	}
-}).listen(3000);
+}).listen(config.port);
 
 localIpV4Address().then(function (ipAddress) {
 	console.log("My IP address is " + ipAddress);
